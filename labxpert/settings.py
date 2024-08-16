53350f8681bde9b980968c3e2d04a1ace7a423a2
GaneshMacharla <ganeshmacharla2020@gmail.com>
2024-08-16 22:00:21 +0530

    added ai

--- labxpert/settings.py
+++ labxpert/settings.py
@@ -18,7 +18,6 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-OPEN_AI_API_KEY="sk-proj-VeWYwDWuE2DKSUTioW0kT3BlbkFJ5pz10iT4B1doloWel3bq"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
